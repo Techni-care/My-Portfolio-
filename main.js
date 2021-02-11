3be95ad3209f2fc19d9691d231227ebbabbfb089
Techni-care <78097540+Techni-care@users.noreply.github.com>
2021-02-10 23:49:35 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+});
+
+
+sr.reveal('.intro',{}); 
+sr.reveal('.buttn',{delay: 270}); 
+sr.reveal('.mypic',{delay: 470}); 
+sr.reveal('.mypic2',{delay: 470});
+sr.reveal('.intro h1 h2',{ interval: 200}); 
+
+
+sr.reveal('.abt',{}); 
+sr.reveal('.about',{delay: 408}); 
+sr.reveal('.about3',{delay: 402}); 
+
+
+sr.reveal('.skillssubtitle',{}); 
+sr.reveal('.skillstext',{}); 
+sr.reveal('.skillsdata',{interval: 200}); 
+sr.reveal('.skillsimg',{delay:250});
+ 
+
+
+//sr.reveal('.contact1',{interval: 200}); 
+sr.reveal('.cont',{interval: 500}); 
+sr.reveal('.click',{interval: 470}); 
+sr.reveal('.almost',{interval: 100}); 
+
+
+
+
